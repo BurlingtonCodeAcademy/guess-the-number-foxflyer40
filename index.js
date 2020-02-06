cd3552f58cbb8f33a0dac3ebb4adb136fc03012a
Michael Muzzy <foxflyer40@gmail.com>
2020-02-06 16:14:20 -0500

    pick a num and let computer win passed

--- index.js
+++ index.js
@@ -7,12 +7,33 @@ function ask(questionText) {
   });
 }
 
+//random number generator
+function randomInt(maxNum,minNum){
+    return Math.floor(minNum + (Math.random() * (maxNum - minNum + 1)))
+}
+
+
 start();
+// initialize game variables
+let HighNum = 5
+let LowNum = 1
+let numGuessed = randomInt(HighNum, LowNum)
+
 
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
   // Now try and complete the program.
-  process.exit();
+
+ // ask human to confirm guess
+ let humanAnswer = await ask("Is your number " + numGuessed + "?\n")
+console.log(humanAnswer)
+ 
+if (humanAnswer === "y") {
+  console.log("YIPEE !!!\nYou picked " + secretNumber +"\nI guessed " + numGuessed)
+} 
+ 
+ 
+    process.exit();
 }
