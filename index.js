18a2e782b445e8f55bcda92ab1c0d1ae7cd8c688
Michael Muzzy <foxflyer40@gmail.com>
2020-02-07 15:25:55 -0500

    added trick tracking

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ function randomInt(maxNum, minNum) {
 }
 
 start();
-// initialize game variables
+// initialize global game variables
 let highNum = 100;
 let lowNum = 1;
 let humanAnswer = "";
+let numberOfTries = 0
+let humanTricks = 0
 
 
 async function start() {
@@ -39,29 +41,36 @@ async function start() {
     if (newGuessed === lowNum) {
       newGuessed += 1
     }
-
+    // iterate number of tries
+    numberOfTries += 1
     // Guess number and ask human to confirm guess
-    humanAnswer = await ask("\nIs your number " + newGuessed + "?\n");
+    humanAnswer = await ask("Is your number " + newGuessed + "?\n");
     // respond to Y/N - quit if Y ask high or low if N -
     if (humanAnswer === "y") {
-      console.log(
-        "YIPEE !!!\nYou picked " + secretNumber + "\nI guessed " + newGuessed
-      );
+      console.log("YIPEE !!!\nI guessed it in " + numberOfTries + " tries!");
+      if (humanTricks >= 1) {
+        console.log("(However, you tried to trick me " + humanTricks + " times.)\nSad.")
+      }
       process.exit();
+    } else if (humanAnswer !== "n") {
+      // do this if human cannot follow instructions
+      console.log("Please answer 'y' or 'n'\nLet's try again...")
+      humanTricks += 1
     } else {
       let highLowIn = await ask(
-        "Is your number higher or lower than my guess?\n(Please enter H or L)\n"
+        "Is your number higher or lower than " + newGuessed + "?\n(Please enter H or L)\n"
       );
-      // if H store guess as lowNum if L store guess as highNum 
+      // if H, store guess as lowNum - if L, store guess as highNum 
       if (highLowIn === "h") {
         lowNum = newGuessed;
       } else if (highLowIn === "l") {
         highNum = newGuessed;
         // do this if human cannot follow instructions
       } else {
-        console.log("I don't think so Buster.\nLet's Try again...");
+        console.log("I don't think so Buster.\nTry again...");
+        humanTricks += 1
       }
-      
+
       //ask again
       console.log("\nOk.  Let's see.... Hmmm...");
     }
