562336c1090b3c9c769d2a9eb17c94cfdf527940
Michael Muzzy <foxflyer40@gmail.com>
2020-02-07 13:36:06 -0500

    fixed duplicate guesses

--- index.js
+++ index.js
@@ -33,9 +33,19 @@ async function start() {
   // create game loop
   while (humanAnswer !== "y") {
     // Guess number and ask human to confirm guess
-        let newGuessed = randomInt(highNum, lowNum);
-    humanAnswer = await ask("Is your number " + newGuessed + "?\n");
-    console.log("\n");
+    let newGuessed = randomInt(highNum, lowNum);
+    if (newGuessed === highNum) {
+      newGuessed -= 1
+    }
+    if (newGuessed === lowNum) {
+      newGuessed += 1
+    }
+
+
+
+
+    humanAnswer = await ask("\nIs your number " + newGuessed + "?\n");
+
 
     // respond to Y/N - quit if Y ask high or low if N -
     if (humanAnswer === "y") {
@@ -43,30 +53,30 @@ async function start() {
         "YIPEE !!!\nYou picked " + secretNumber + "\nI guessed " + newGuessed
       );
       // track value of variables
-      console.log(humanAnswer + " = humanAnswer");
+      console.log(secretNumber + " = secretNumber");
       console.log(highNum + " = highNum value");
       console.log(lowNum + " = lowNum value");
       console.log(newGuessed + " = newGuessed");
       console.log(lastGuessed + " = lastGuessed");
       process.exit();
     } else {
-      
+
       let highLowIn = await ask(
-        "Is your number higher or lower than my guess?\n(Please enter H or L)\n"
+        "\nIs your number higher or lower than my guess?\n(Please enter H or L)\n"
       );
-      // if H store guess as lowNum if L store guess as Hi num
+      // if H store guess as lowNum if L store guess as highNum
       if (highLowIn === "h") {
         lastGuessed = newGuessed
         lowNum = newGuessed;
-          } else if (highLowIn === "l") {
-            lastGuessed = newGuessed
-            highNum = newGuessed;
-         } else {
-      console.log("I don't think so Buster.\n");
+      } else if (highLowIn === "l") {
+        lastGuessed = newGuessed
+        highNum = newGuessed;
+      } else {
+        console.log("I don't think so Buster.\nTry again...");
       }
       // track value of variables
-      console.log(humanAnswer + " = humanAnswer");
-      console.log(highLowIn + " = highLowIn");
+      console.log("\n")
+      console.log(secretNumber + " = secretNumber");
       console.log(highNum + " = highNum value");
       console.log(lowNum + " = lowNum value");
       console.log(newGuessed + " = newGuessed");
