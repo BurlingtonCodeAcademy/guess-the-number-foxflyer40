851534defd358004dca0d7572d1cd50adc3ba058
foxflyer40 <60403841+foxflyer40@users.noreply.github.com>
2020-02-25 09:07:19 -0500

    clean up code to reassess-3

--- index.js
+++ index.js
@@ -13,21 +13,20 @@ function randomInt(maxNum, minNum) {
 }
 
 start();
-// initialize global game variables
-let highNum = 100;
-let lowNum = 1;
-let humanAnswer = "";
-let numberOfTries = 0
-let humanTricks = 0
-
 
 async function start() {
-  console.log(
-    "\nLet's play a game where you (human),\npick a secret number between 1 and 100,\nand I (computer) try to guess it.\nOK?\n"
-  );
+  // initialize global game variables
+  let highNum = 100;
+  let lowNum = 1;
+  let humanAnswer = "";
+  let numberOfTries = 0
+  let humanTricks = 0
+  console.log("\nLet's play a game where you (human),\npick a secret number between 1 and 100,\nand I (computer) try to guess it.\nOK?\n");
   let secretNumber = await ask(
     "What is your secret number?\nI won't peek, I promise...\n"
   );
+console.log(highNum) + '1';
+console.log(lowNum);
 
   // create game loop OR error out if non number, decimal number or number out of range is entered
   if ((secretNumber % 1 === 0) && (secretNumber >= lowNum && secretNumber <= highNum)) {
@@ -38,7 +37,8 @@ async function start() {
   }
 
   while (humanAnswer !== "y") {
-
+    console.log(highNum) + '2';
+    console.log(lowNum);
     // create new guess + rectify if = high or low number + iterate number of tries
     let newGuessed = randomInt(highNum, lowNum);
     if (newGuessed === highNum) {
@@ -60,9 +60,7 @@ async function start() {
         console.log("(MAYBE you made a mistake here and there,\nHowever, I think you tried to trick me " + humanTricks + " times.)\nSad.")
       }
       process.exit();
-
-      // verify "n" answer AND check for wrong input with !==n
-    } else if (humanAnswer !== "n") {
+    } else if (humanAnswer !== "n") {  // verify "n" answer AND check for wrong input with !==n
       // error message and re-guess if NOT n + iterate humanTricks counter
       console.log("\nUh-O!\nPlease use h or l only.\nLet me guess again...\n")
       humanTricks += 1
@@ -71,9 +69,7 @@ async function start() {
       /* if H, store guess as lowNum  -OR- 
         if L, store guess as highNum -OR- 
         if entry is not H or L give error message and re-guess */
-      let highLowIn = await ask(
-        "Is your number higher or lower than " + newGuessed + "?\n(Please enter h or l)\n"
-      );
+      let highLowIn = await ask("Is your number higher or lower than " + newGuessed + "?\n(Please enter h or l)\n")
       if (highLowIn === "h") {
         lowNum = newGuessed;
       } else if (highLowIn === "l") {
@@ -86,6 +82,7 @@ async function start() {
       //Guess again
       console.log("\nOk.  Let's see.... Hmmm...");
     }
+
   }
   process.exit();
 }
